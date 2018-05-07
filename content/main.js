dae89f39a8997684abb74b306d8d8b16692e7c21
Suz Hinton <suz.hinton@gmail.com>
2018-05-07 13:24:08 -0400

    write first sample for testing

---
+++ content/main.js
@@ -0,0 +1,43 @@
+// https://docs.microsoft.com/en-us/azure/iot-hub/quickstart-send-telemetry-node
+const connectionString = '{Your device connection string here}';
+const Mqtt = require('azure-iot-device-mqtt').Mqtt;
+const DeviceClient = require('azure-iot-device').Client
+const Message = require('azure-iot-device').Message;
+const client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+
+const tessel = require('tessel');
+const ambientlib = require('ambient-attx4');
+const ambient = ambientlib.use(tessel.port['A']);
+
+ambient.on('ready', () => {
+  // Get points of light data. The readings will happen every .5 seconds
+  setInterval(() => getLightLevel, 500);
+});
+
+ambient.on('error', (err) => {
+  console.log(err);
+});
+
+// Set up the handler for the LED direct method call.
+client.onDeviceMethod('toggleLED', () => {
+  tessel.led[2].toggle();
+});
+
+function getLightLevel() {
+  ambient.getLightLevel((err, lightdata) => {
+    if (err) throw err;
+    console.log('Light level:', lightdata.toFixed(8));
+    reportLightLevel(lightdata);
+  });
+};
+
+function reportLightLevel(value) {
+  const data = JSON.stringify({light: value });
+  const message = new Message(data);
+
+  console.log('Sending message: ' + message.getData());
+  client.sendEvent(message, (error, result) => {
+    console.log('message sent');  
+  });
+}
+
